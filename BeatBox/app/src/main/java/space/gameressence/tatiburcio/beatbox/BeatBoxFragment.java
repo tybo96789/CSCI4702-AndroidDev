feffc933f61db4109fa0cece0c6912b46a181c52
Tyler Atiburcio <tyler.atiburcio@gmail.com>
2018-02-12 22:22:14 -1000

    New Project-BeatBox; Complier issues resolved: Wrong Layout class

--- BeatBox/app/src/main/java/space/gameressence/tatiburcio/beatbox/BeatBoxFragment.java
+++ BeatBox/app/src/main/java/space/gameressence/tatiburcio/beatbox/BeatBoxFragment.java
@@ -1,13 +1,34 @@
 package space.gameressence.tatiburcio.beatbox;
 
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import space.gameressence.tatiburcio.beatbox.databinding.FragmentBeatBoxBinding;
 
 public class BeatBoxFragment extends Fragment {
 
+    //buildToolsVersion '27.0.3'
 
     public static BeatBoxFragment newInstance() {
         return new BeatBoxFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        FragmentBeatBoxBinding binding = DataBindingUtil
+                .inflate(inflater, R.layout.fragment_beat_box, container, false);
+
+        binding.recyclerView.setLayoutManager(new GridLayoutManager(getActivity(),
+                3));
+
+        return binding.getRoot();
     }
 }
